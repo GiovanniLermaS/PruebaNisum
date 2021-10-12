9cfbb67900a5cd564275e18b82639ec7e2e1e5be
Giovanni Lerma Suarez <giosuarezler@gmail.com>
2021-10-12 13:37:02 -0500

    Response models added.

---
+++ app/src/main/java/com/example/pruebanisum/db/model/Lf.kt
@@ -0,0 +1,8 @@
+package com.example.pruebanisum.db.model
+
+class Lf {
+    val lf: String? = null
+    val freq: Int? = null
+    val since: Int? = null
+    val vars = ArrayList<Lf>()
+}
---
+++ app/src/main/java/com/example/pruebanisum/db/model/ResponseService.kt
@@ -0,0 +1,6 @@
+package com.example.pruebanisum.db.model
+
+class ResponseService {
+    val sf: String? = null
+    val lfs = ArrayList<Lf>()
+}
